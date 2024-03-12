a112030360db6c45fa35dfa3875243eaf1570849
sticktostack <rajdeep6153@gmail.com>
2024-03-13 01:45:30 +0530

    second commit

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-let resultBox = document.querySelector('#resultbox')
-let buttons = document.querySelectorAll('button')
-let allClear = document.querySelector('#allclear')
+let resultBox = document.querySelector("#resultbox");
+let buttons = document.querySelectorAll("button");
+let allClear = document.querySelector("#allclear");
 
+let result = "";
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "AC") {
+      resultBox.value = "";
+    } else if (e.target.innerHTML === "=") {
+      result = eval(result);
+      resultBox.value = result;
+    } else if (e.target.value === "del") {
+      result = result.substring(0, result.length - 1);
+      resultBox.value = result;
+    } else {
+      resultBox.value = result += button.value;
+    }
+  });
+});
 
-let result = ''
-buttons.forEach((button)=>{
-    button.addEventListener('click',()=>{
-        resultBox.value = result += button.value 
-        allClear.addEventListener('click',()=>{
-            resultBox.value = ''
-            result = ''
-        })
-      
-        console.log(button.value);
-    })
-})
-
- 
