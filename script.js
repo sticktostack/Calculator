cb018deec7d8c446bcfbaac106b18a9af87fb093
sticktostack <rajdeep6153@gmail.com>
2024-03-13 17:57:44 +0530

    semi final commit

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let resultBox = document.querySelector("#resultbox");
 let buttons = document.querySelectorAll("button");
-let allClear = document.querySelector("#allclear");
-
+let und = undefined
 let result = "";
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    try {
-        if (e.target.innerHTML === "AC") {
-            resultBox.value = "";
-          } else if (e.target.innerHTML === "=") {
-            result = eval(result);
-            resultBox.value = result;
-          }
-           else {
-            resultBox.value = result += button.value;
-          }
-    } catch (error) {
-        console.log(error);
+    if (e.target.id === "back") {
+      result = result.substring(0, result.length - 1);
+      resultBox.value = result;
+    } else if (e.target.innerHTML === "=") {
+      result = eval(result);
+      resultBox.value = result;
+    } else if (e.target.id === "allclear") {
+      result = "";
+      resultBox.value = result;
+    }else if(resultBox.value == 'undefined'){
+      resultBox.value = 0
+    }else {
+      result += e.target.innerHTML;
+      resultBox.value = result;
     }
   });
 });
-
