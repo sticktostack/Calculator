440216e73ccdabf43acb6a5e92cf02dce7696726
sticktostack <rajdeep6153@gmail.com>
2024-03-14 15:11:38 +0530

    final commit

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 let resultBox = document.querySelector("#resultbox");
 let buttons = document.querySelectorAll("button");
-let und = undefined
 let result = "";
+
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    if (e.target.id === "back") {
-      result = result.substring(0, result.length - 1);
-      resultBox.value = result;
-    } else if (e.target.innerHTML === "=") {
-      result = eval(result);
-      resultBox.value = result;
-    } else if (e.target.id === "allclear") {
-      result = "";
-      resultBox.value = result;
-    }else if(resultBox.value == 'undefined'){
-      resultBox.value = 0
-    }else {
-      result += e.target.innerHTML;
-      resultBox.value = result;
+
+    try {
+
+      if (e.target.id === "back") {
+        result = result.substring(0, result.length - 1);
+        resultBox.value = result;
+      } else if (e.target.innerHTML === "=") {
+        result = eval(result).toString();
+        resultBox.value = result;
+      } else if (e.target.id === "allclear") {
+        result = "";
+        resultBox.value = result;
+      }else if(resultBox.value == 'undefined'){
+        resultBox.value = 0
+      }else {
+        result += e.target.innerHTML;
+        resultBox.value = result;
+      }
+      if (result === und) {
+        resultBox.value = "0";
+      }
+    } //try blocks end 
+    
+    catch (error) {
+      console.log(error);
     }
   });
 });
